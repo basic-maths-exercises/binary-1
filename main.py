5234390fe2e760e779c1423f39abbe9ba3d4aec9
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-23 15:15:26 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def compute_b( digits ) :
+  b = 0
+  # Your code goes here
+  
+  return b
+  
+# You should not need to modify the code from here
+# this is to test the code that you have written.
+print( compute_b([1,1,1]) )
+print( compute_b([0,1,1]) )
+print( compute_b([1,0,1]) )
+print( compute_b([0,0,1]) )
+print( compute_b([1,1,0]) )
+print( compute_b([0,1,0]) )
+print( compute_b([1,0,0]) )
+print( compute_b([0,0,0]) )
